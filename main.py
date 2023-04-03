12ef68fd91a4a44cfeb1c315a62177adc214d8e7
João Gabriel <128388946+JoaoGAO@users.noreply.github.com>
2023-04-03 11:37:49 -0300

    Correção

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 idade=int(input("digite sua idade:"))
 if idade < 0 :
- print("impossivel")
+ print("impossivel!")
 elif idade < 18:
- print("não precisa se alistar")
+ print("não precisa se alistar.")
 elif 65> idade >18:
- print("não esqueça de votar na proxima eleição")
+ print("não esqueça de votar na proxima eleição.")
 elif idade > 65:
- print("vá descançar")
+ print("vá descançar.")
 else:
  print("eita!")
