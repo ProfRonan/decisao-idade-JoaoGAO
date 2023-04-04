00993361dd6f47186eb4bff8b3cd707ad1990b74
João Gabriel <128388946+JoaoGAO@users.noreply.github.com>
2023-04-04 00:15:01 +0000

    Correção

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 idade=int(input("digite sua idade:"))
 if idade < 0 :
  print("impossível!")
+ print("não precisa se alistar.")
 elif idade < 18:
  print("não precisa se alistar.")
 elif 65> idade >18:
